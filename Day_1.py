a2f60a6cb108e2b505edeab069893c6b9aa2eb79
Outbounderic <ericvanmeter57@gmail.com>
2021-12-11 21:46:55 -0600

    Finished up daily reading

--- Day_1.py
+++ Day_1.py
@@ -3,3 +3,8 @@
 # string concatenation
 greet = "Hello World! \n"
 print(greet + greet + greet)
+# utilize the console for debugging
+# input funtion outputs a message and requests a response
+print("It's " + input("What do you think about green?"))
+# python variables
+# make code readable and easy to return to
