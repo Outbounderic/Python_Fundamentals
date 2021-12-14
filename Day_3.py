a1f305fea94e582c3c376d8f0e2f6547713a8364
Outbounderic <ericvanmeter57@gmail.com>
2021-12-14 10:04:55 -0600

    Conditional treasure hunter game finished.

--- Day_3.py
+++ Day_3.py
@@ -27,3 +27,33 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
+# input -> conditional 2
+# input -> conditional 2
+# input -> condiiional 4
+
+print("You reach a coast; you can go around through the treacherous jungle or you can take your time and swim.")
+first_choice = input('Jungle or Swim ').lower()
+
+if first_choice == 'jungle':
+    print("As you hike through the jungle you trip on a hidden branch and fall in to a hole. \nGame Over.")
+    exit()
+elif first_choice == 'swim':
+    print("You take your time and swim, the sun is perfect and water is clear. You see a school of fish ahead.")
+    second_choice = input('Swim or Wait ').lower()
+    if second_choice == 'swim':
+        print('As you swim through the school of fish you are attacked by a shark. \nGame Over.')
+        exit()
+    elif second_choice == 'wait':
+        print('You wait and are rewarded for your patience. A shark swims through the school looking for a snack. It passes and you make it to the shore.')
+        print('You trek through the jungle for hours until you come across three stone doors.')
+        third_choice = input('Which do you choose: Red, Blue, or Yellow. ').lower()
+        if third_choice == 'red':
+            print('As you walk into the red door you fall into a pit of fire. \nGame Over')
+            exit()
+        elif third_choice == 'blue':
+            print('Passing the blue door it slams shut, several tigers attack you. \nGame Over')
+            exit()
+        elif third_choice == 'yellow':
+            print('Walking past the yellow door you are greeted with a magnificent chest! \nYou Win!')
+        else:
+            print('After this long day no one blames you from turning around. \nGame Over')
