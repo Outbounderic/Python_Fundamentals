b16883889a4921393a8e49604eb3ed02a70a61ff
Outbounderic <ericvanmeter57@gmail.com>
2021-12-15 15:44:28 -0600

    Added password generator starter.

--- Day_5.py
+++ Day_5.py
@@ -1,17 +1,17 @@
-# 🚨 Don't change the code below 👇
-# student_heights = input("Input a list of student heights ").split()
-# for n in range(0, len(student_heights)):
-#   student_heights[n] = int(student_heights[n])
-# 🚨 Don't change the code above 👆
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 
-# #Write your code below this row 👇
-
-# for n in student_heights:
-#     total_sum = 0
-#     total_sum = total_sum + (student_heights[n] - 1)
-#     print(total_sum)
-#     if n == (len(student_heights) -1):
-#         print(round(total_sum / len(student_heights)))
-
-
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
