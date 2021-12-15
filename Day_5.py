258f9d94723530ac948909259202570d291e84d2
Outbounderic <ericvanmeter57@gmail.com>
2021-12-15 15:25:24 -0600

    Commented code out for later.

--- Day_5.py
+++ Day_5.py
@@ -1,17 +1,17 @@
 # 🚨 Don't change the code below 👇
-student_heights = input("Input a list of student heights ").split()
-for n in range(0, len(student_heights)):
-  student_heights[n] = int(student_heights[n])
+# student_heights = input("Input a list of student heights ").split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
 # 🚨 Don't change the code above 👆
 
 
-#Write your code below this row 👇
+# #Write your code below this row 👇
 
-for n in student_heights:
-    total_sum = 0
-    total_sum = total_sum + (student_heights[n] - 1)
-    print(total_sum)
-    if n == (len(student_heights) -1):
-        print(round(total_sum / len(student_heights)))
+# for n in student_heights:
+#     total_sum = 0
+#     total_sum = total_sum + (student_heights[n] - 1)
+#     print(total_sum)
+#     if n == (len(student_heights) -1):
+#         print(round(total_sum / len(student_heights)))
 
 
