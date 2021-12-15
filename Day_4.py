ee0a2d78d2d01bad260bf8f432e9710c7efa23e2
Outbounderic <ericvanmeter57@gmail.com>
2021-12-14 19:30:27 -0600

    Basic list practice.

---
+++ Day_4.py
@@ -0,0 +1,20 @@
+# import random
+
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia"]
+# print(states_of_america[0])
+
+import random
+
+# Split string method
+# names_string = input("Give me everybody's names, separated by a comma. ")
+# names = names_string.split(", ")
+# # 🚨 Don't change the code above 👆
+
+# #Write your code below this line 👇
+
+# random_integer = random.randint(0, (len(names) - 1))
+# print(random_integer)
+# print(f"{names[random_integer]} is going to buy the meal today!")
