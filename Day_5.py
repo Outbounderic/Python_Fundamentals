35be52554009e03d824827250f78f5a2ca5ac087
Outbounderic <ericvanmeter57@gmail.com>
2021-12-15 18:59:33 -0600

    Started random gen for list.

--- Day_5.py
+++ Day_5.py
@@ -5,13 +5,20 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+# for loop
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+rand_letters = random.randint(1, (len(letters) - 1))
+rand_symbols = random.randint(1, (len(symbols) - 1))
+rand_numbers = random.randint(1, (len(numbers) - 1))
 
+# for x in letters:
+#     print(x)
 
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+# for x in symbols:
+#     print(x)
+
+# for x in numbers:
+#     print(x)
