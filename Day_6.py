be22e06024a751ec0b387f2fd42056f5929a1c91
Outbounderic <ericvanmeter57@gmail.com>
2021-12-17 14:33:50 -0600

    Small edit for clarity.

--- Day_6.py
+++ Day_6.py
@@ -1,17 +1,3 @@
-"""
-The game of hangman
--------------------
-Game Start and the menu is printed.
-The word is randomly selected from a list.
-Create blanks for word length.
-Ask for a guess from the player selecting one letter.
-Check for letter in word.
-Check if correct: Yes, add letter to board; No, add hangman piece.
-Check if blanks are all filled in: Yes, you win; No, back to start.
-Check if player has remaining lives: Yes, you lose; No, back to start
--------------------
-"""
-
 import random
 
 stages = ['''
