4a81229bb6c01227bf7c9ebce98daf518436fcb9
Outbounderic <ericvanmeter57@gmail.com>
2021-12-17 14:28:57 -0600

    Fixed bug in lives and connected ASCII to guesses.

--- Day_6.py
+++ Day_6.py
@@ -87,6 +87,12 @@ while not end_of_game:
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
+
+    if guess not in chosen_word:
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")
         
 #TODO-2: - If guess is not a letter in the chosen_word,
     #Then reduce 'lives' by 1. 
@@ -97,9 +103,9 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win!")
-    elif lives == 0:
-        print("You lose.")
-        break
+
+
+    print(stages[lives])
         
 #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
 
