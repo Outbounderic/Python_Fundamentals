d42b0b868ff0b1f1b4d0b4ada3edc637c01eade9
Outbounderic <ericvanmeter57@gmail.com>
2021-12-17 08:36:24 -0600

    Psuedo-code for hangman

--- Day_6.py
+++ Day_6.py
@@ -1,5 +1,14 @@
-def my_function():
-    print("Hello")
-    print("Bye")
+"""
+The game of hangman
+-------------------
+Game Start and the menu is printed.
+The word is randomly selected from a list.
+Create blanks for word length.
+Ask for a guess from the player selecting one letter.
+Check for letter in word.
+Check if correct: Yes, add letter to board; No, add hangman piece.
+Check if blanks are all filled in: Yes, you win; No, back to start.
+Check if player has remaining lives: Yes, you lose; No, back to start
+-------------------
+"""
 
-my_function()
