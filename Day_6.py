4a12d41a8d458f6e230a99cb4f0a862850e883f9
Outbounderic <ericvanmeter57@gmail.com>
2021-12-17 11:37:32 -0600

    Stuck on losing lives.

--- Day_6.py
+++ Day_6.py
@@ -71,26 +71,37 @@ stages = ['''
 =========
 ''']
 
+end_of_game = False
+lives = 6
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 display = []
-count = 0
 word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
-    count += 1
 
-while count > 0:
+while not end_of_game:
     guess = input("Guess a letter: \n").lower()
 
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
-            count -= 1
-    print(display)
         
+#TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
+    print(f"{' '.join(display)}")  
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+    elif lives == 0:
+        print("You lose.")
+        break
+        
+#TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
 
 
 
