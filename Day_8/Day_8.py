6a4ef59108bc1ba5676261bd4cdf1b030569b160
Outbounderic <ericvanmeter57@gmail.com>
2021-12-19 13:47:26 -0600

    Working with Args and Kwargs.

---
+++ Day_8/Day_8.py
@@ -0,0 +1,10 @@
+formal_name = "John"
+city = "Los Angelos"
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print("How do you do?")
+    print(f"Isn't the weather nice in {location} today?")
+
+greet_with(formal_name, city)
+    
