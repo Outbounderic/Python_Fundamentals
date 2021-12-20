6aa61a510c97e9c6c44e7fd93583febf661b7907
Outbounderic <ericvanmeter57@gmail.com>
2021-12-19 20:46:29 -0600

    Created divisibility calculator.

--- Day_8/Day_8.py
+++ Day_8/Day_8.py
@@ -1,10 +1,14 @@
-formal_name = "John"
-city = "Los Angelos"
 
-def greet_with(name, location):
-    print(f"Hello {name}")
-    print("How do you do?")
-    print(f"Isn't the weather nice in {location} today?")
+# def prime_checker(number):
+#     divisibilty = []
+#     for x in range(number):
+#         if number % (x + 1) == 0:
+#             divisibilty.append(x + 1)
+#     if len(divisibilty) == 2:
+#         print("It's a prime number.")
+#     else:
+#         print("It's not a prime number.")
+#     print(divisibilty)
 
-greet_with(formal_name, city)
-    
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
