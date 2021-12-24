9671a9796af92697d2d96787726d5aed9b3c6e22
Outbounderic <ericvanmeter57@gmail.com>
2021-12-24 13:40:38 -0600

    Started secret auction project.

--- Day_9/Day_9.py
+++ Day_9/Day_9.py
@@ -1,30 +1,9 @@
-# programming_dictionary = {
-#     "Bug": "An error in a program that prevents the program from running as expected.",
-#     "Function": "A piece of code that you can easily call over and over again.",
-#     "Loop": "The action of doing something over and over again.",
-#     "Number": 2,
-# }
+from art import logo
 
-# for key in programming_dictionary:
-#     print(key)
-#     print(programming_dictionary[key])
+print(logo)
+print("Welcome to the secret auction program.")
 
-# capitals = {
-#     "France": "Paris",
-#     "Germany": "Berlin",
-# }
-
-travel_log = [
-    {
-        "country": "France",
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "total_visits": 12,
-    },
-    {
-        "country": "Germany",
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "total_visits": 5,
-    },
-]
-
-print(travel_log[0]["cities_visited"][0])
+# input on if there are more bidders
+# if yes gather name, key, and their bid, value
+# if no loop over bidders and find highest bid
+# display the largest bid
