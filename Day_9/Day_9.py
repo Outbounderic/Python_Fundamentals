536e281a58111a6ea06eae58c40a40588d14bb99
Outbounderic <ericvanmeter57@gmail.com>
2021-12-24 10:49:42 -0600

    Working with nesting.

--- Day_9/Day_9.py
+++ Day_9/Day_9.py
@@ -1,1 +1,22 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# programming_dictionary = {
+#     "Bug": "An error in a program that prevents the program from running as expected.",
+#     "Function": "A piece of code that you can easily call over and over again.",
+#     "Loop": "The action of doing something over and over again.",
+#     "Number": 2,
+# }
+
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+
+capitals = {
+    "France": "Paris",
+    "Germany": "Berlin",
+}
+
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lille", "Dijon"],},
+    "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+}
+
+print(travel_log["France"]["cities_visited"])
