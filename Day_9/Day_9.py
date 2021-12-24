5a9ee0de0e2cdd8c6f57b9408ad57e26a0717575
Outbounderic <ericvanmeter57@gmail.com>
2021-12-24 13:57:29 -0600

    Created auction function.

--- Day_9/Day_9.py
+++ Day_9/Day_9.py
@@ -3,7 +3,17 @@ from art import logo
 print(logo)
 print("Welcome to the secret auction program.")
 
-# input on if there are more bidders
+bid_list = {}
+
+def auction_entry():
+    bid_name = input("What is your name?: ")
+    bid_amount = input("What's your bid?: ")
+    bid_list[bid_name] = int(bid_amount)
+
+#TODO-1 input on if there are more bidders
 # if yes gather name, key, and their bid, value
 # if no loop over bidders and find highest bid
 # display the largest bid
+
+auction_entry()
+print(bid_list)
