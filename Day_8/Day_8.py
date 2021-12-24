b0d1115076df5ca934f6f486f6a7c42c6f363fce
Outbounderic <ericvanmeter57@gmail.com>
2021-12-23 20:04:19 -0600

    Modified for decryption.

--- Day_8/Day_8.py
+++ Day_8/Day_8.py
@@ -6,31 +6,36 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-#e.g. 
-#plain_text = "hello"
-#shift = 5
-#cipher_text = "mjqqt"
-#print output: "The encoded text is mjqqt"
-
-##HINT: How do you get the index of an item in a list:
-#https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
-
 cypher_alphabet = collections.deque(alphabet)
 
-def encrypt(text, shift):
-    cypher_alphabet.rotate(shift)
+def encrypt(plain_text, shift_amount):
+    cypher_alphabet.rotate(-shift_amount)
     encrypted_text = []
-    for letter in text:
+    for letter in plain_text:
         for key in alphabet:
             if letter == key:
                 altered_letter = alphabet.index(letter)
                 encrypted_text.append(cypher_alphabet[altered_letter])
                 
-    print("".join(encrypted_text))
+    print(f"The encoded text is {''.join(encrypted_text)}")
 
-encrypt(text, shift)
+  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
+  #e.g. 
+  #cipher_text = "mjqqt"
+  #shift = 5
+  #plain_text = "hello"
+  #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+
+def decrypt(plain_text, shift_amount):
+    cypher_alphabet.rotate(-shift_amount)
+    decrypted_text = []
+    for letter in plain_text:
+        for key in alphabet:
+            if letter == key:
+                altered_letter = alphabet.index(letter)
+                decrypted_text.append(cypher_alphabet[altered_letter])
+                
+    print(f"The decoded text is {''.join(decrypted_text)}")
