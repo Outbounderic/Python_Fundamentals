ea9017f352ebbec8f149601d8c0413b8a47428d0
Outbounderic <ericvanmeter57@gmail.com>
2021-12-25 20:32:32 -0600

    Added use loop.

--- Day_10/Day_10.py
+++ Day_10/Day_10.py
@@ -1,5 +1,8 @@
 from art import logo
 
+continue_calculating = True
+
+
 def add(a, b):
     return a + b
 
@@ -20,4 +23,19 @@ operations = {
 }
 
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
+
+for symbol in operations:
+    print(symbol)
+
+while continue_calculating:
+    operation_symbol = input("Pick and operation: ")
+    num2 = int(input("What's the next number?: "))
+
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    num1 = answer
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    continue_with_answer = input(f"Type 'y' to continue with {answer}, or type 'n' to exit.: ")
+    if continue_with_answer == 'n':
+        continue_calculating = False
