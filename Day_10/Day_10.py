bf20de0be10e08d008157e6e837f94e6afe7ab52
Outbounderic <ericvanmeter57@gmail.com>
2021-12-25 19:48:47 -0600

    Created dictionary with operations.

--- Day_10/Day_10.py
+++ Day_10/Day_10.py
@@ -1,1 +1,23 @@
 from art import logo
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
