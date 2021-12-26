3150d1f2a3b949e08cf29f5036499cd9a5547102
Outbounderic <ericvanmeter57@gmail.com>
2021-12-25 19:02:24 -0600

    Added function escape.

--- Day_10/Day_10.py
+++ Day_10/Day_10.py
@@ -1,15 +1,20 @@
 # Functions with outputs
 
 def format_name(f_name, l_name):
-    split_first_name = list(f_name.lower())
-    split_first_name[0] = split_first_name[0].upper()
-    split_last_name = list(l_name.lower())
-    split_last_name[0] = split_last_name[0].upper()
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    else:
+        split_first_name = list(f_name.lower())
+        split_first_name[0] = split_first_name[0].upper()
+        split_last_name = list(l_name.lower())
+        split_last_name[0] = split_last_name[0].upper()
 
-    full_name = f"{''.join(split_first_name)} {''.join(split_last_name)}"
-    return full_name
+        full_name = f"{''.join(split_first_name)} {''.join(split_last_name)}"
+
+        return full_name
 
 
 formatted_name = format_name("eRiC", "vanMEter")
 
 print(formatted_name)
+print(format_name("", ""))
