63109da3dfa369792292028c17524c65c39db4c3
Outbounderic <ericvanmeter57@gmail.com>
2021-12-27 16:47:03 -0600

    Psuedo code todo list.

---
+++ Day_11/main.py
@@ -0,0 +1,8 @@
+# Guess the number game with normal and hard modes
+# Computer shows intro
+# Computer asks for difficulty
+# Generate a random number
+# Req input from player
+# Compare if number is higher or lower than guess
+# Res the state and decrement the amount of guesses left
+# Loop over game cycle until guesses left equals zero or guess equals number
