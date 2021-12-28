b4679e5add53daa9f31b79ee80e08fee579a4ee0
Outbounderic <ericvanmeter57@gmail.com>
2021-12-28 15:11:04 -0600

    Finished game loop.

--- Day_11/main.py
+++ Day_11/main.py
@@ -14,6 +14,28 @@ def set_difficulty(res):
     elif res == "hard":
         return guesses + 5
 
-print("Welcome to the number guessing game. I'll pick a number and you will try to guess it, i'll let you know if you are above or below it.")
-guesses = set_difficulty(input("Would you like to play on normal or hard: "))
-print(guesses)
+def compare_guess(res):
+    if res == number_to_guess:
+        print(f"That's correct the number is {number_to_guess}!")
+        print("You win!")
+        exit()
+    elif res < number_to_guess:
+        print(f"The number is higher than {res}.")
+        return guesses - 1
+    elif res > number_to_guess:
+        print(f"The number is lower than {res}.")
+        return guesses - 1
+
+def game_loop():
+    print("Welcome to the number guessing game. I'll pick a number and you will try to guess it, i'll let you know if you are above or below it.")
+    guesses = set_difficulty(input("Would you like to play on normal or hard: "))
+
+    while guesses > 0:
+        compare_guess(int(input(f"Guess a number between 1 and 100: ")))
+        guesses = guesses - 1
+        print(f"You have {guesses} tries left.")
+    
+    if guesses == 0:
+        print("You lose!")
+
+game_loop()
