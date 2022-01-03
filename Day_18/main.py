ade83e4192d70845e2132b7ddcf5d4ae8fd22d9f
Outbounderic <ericvanmeter57@gmail.com>
2022-01-02 20:02:02 -0600

    Finished turtle race.

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,37 +1,36 @@
 from turtle import Turtle, Screen
+import random
+import turtle
 
-
-tim = Turtle()
+is_race_on = False
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bets", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-def move_forwards():
-    tim.forward(10)
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_backwards():
-    tim.backward(10)
+if user_bet:
+    is_race_on = True
 
-def rotate_right():
-    new_heading = tim.heading() + 10
-    tim.setheading(new_heading)
+while is_race_on:
 
-def rotate_left():
-    new_heading = tim.heading() - 10
-    tim.setheading(new_heading)
-
-def clear():
-    tim.clear()
-
-def go_home_tim():
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-screen.listen()
-screen.onkey(move_forwards, "w")
-screen.onkey(move_backwards, "s")
-screen.onkey(rotate_right, "a")
-screen.onkey(rotate_left, "d")
-screen.onkey(clear, "c")
-screen.onkey(go_home_tim, "g")
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won, the winning turtle is {winning_color}!")
+            else:
+                print(f"You've lost, the winning turtle was {winning_color}!")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 screen.exitonclick()
