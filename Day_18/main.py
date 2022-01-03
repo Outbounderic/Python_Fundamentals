67bd1d5256685dc9264b51c9e2b2e37007ef46d3
Outbounderic <ericvanmeter57@gmail.com>
2022-01-02 18:48:49 -0600

    Created etch a sketch.

--- Day_18/main.py
+++ Day_18/main.py
@@ -7,6 +7,31 @@ screen = Screen()
 def move_forwards():
     tim.forward(10)
 
+def move_backwards():
+    tim.backward(10)
+
+def rotate_right():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def rotate_left():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def clear():
+    tim.clear()
+
+def go_home_tim():
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards, "w")
+screen.onkey(move_backwards, "s")
+screen.onkey(rotate_right, "a")
+screen.onkey(rotate_left, "d")
+screen.onkey(clear, "c")
+screen.onkey(go_home_tim, "g")
+
 screen.exitonclick()
