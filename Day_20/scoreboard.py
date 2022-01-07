43f0ccd718061c4b9aeaadb51d5011a1aff4f1d5
Outbounderic <ericvanmeter57@gmail.com>
2022-01-06 22:45:50 -0600

    Finished pong game.

--- Day_20/scoreboard.py
+++ Day_20/scoreboard.py
@@ -12,6 +12,7 @@ class Scoreboard(Turtle):
         
 
     def update_scoreboard(self):
+        self.clear()
         self.goto(-100, 200)
         self.write(self.l_score, align="center", font=("courier", 40, "normal"))
         self.goto(100, 200)
