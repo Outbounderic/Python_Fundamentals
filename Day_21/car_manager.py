322e4d266e879fce063972fbf39c9e7335ff6b4a
Outbounderic <ericvanmeter57@gmail.com>
2022-01-08 13:41:56 -0600

    Added traffic.

--- Day_21/car_manager.py
+++ Day_21/car_manager.py
@@ -7,9 +7,19 @@ MOVE_INCREMENT = 10
 
 class CarManager(Turtle):
     def __init__(self):
-        super().__init__()
-        self.shape("square")
-        self.shapetransform(2, 1)
-        self.penup()
-        self.color(random.choice(COLORS))
-        self.goto(0, random.randint(-260, 280))
+        self.all_cars = []
+
+    def create_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
