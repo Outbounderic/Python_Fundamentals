877898c42b52c5cdc3cde9198030bdbf56c7e4b9
Eric <ericvanmeter57@gmail.com>
2022-01-18 20:26:36 -0600

    Added started files for phonetic alphabet.

--- Day_26/main.py
+++ Day_26/main.py
@@ -1,35 +1,27 @@
-# new_list = [new_item for item in list if test]
-
-# numbers = range(1,5)
-# new_numbers = [number * 2 for number in numbers]
-# print(new_numbers)
-
-# new_dict = {new_key:new:value for item in list if test}
-# new_dict = {new_key:new:value for (key,value) in dict.items()}
-# import random
-# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
-
-# students_scores = {student:random.randint(1,100) for student in names}
-# passed_students = {student:score for (student, score) in students_scores.items() if score > 60}
-
-# print(students_scores)
-# print(passed_students)
-
 student_dict = {
-    "student": ["Angela", "James", "Lily"],
+    "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
 
-# for (key, value) in student_dict.items():
-#     print(value)
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
 
 import pandas
-
 student_data_frame = pandas.DataFrame(student_dict)
-# print(student_data_frame)
 
-# for (key, value) in student_data_frame.items():
-#     print(value)
-
+#Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    print(row.student)
+    #Access index and row
+    #Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+#TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
