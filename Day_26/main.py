8cf5897ef8ce8dcbedb9c66ff7c211c67454bbc7
Eric <ericvanmeter57@gmail.com>
2022-01-18 20:22:01 -0600

    Finished working on dict comprehension.

--- Day_26/main.py
+++ Day_26/main.py
@@ -6,11 +6,30 @@
 
 # new_dict = {new_key:new:value for item in list if test}
 # new_dict = {new_key:new:value for (key,value) in dict.items()}
-import random
-names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
+# import random
+# names = ['Alex', 'Beth', 'Caroline', 'Dave', 'Eleanor', 'Freddie']
 
-students_scores = {student:random.randint(1,100) for student in names}
-passed_students = {student:score for (student, score) in students_scores.items() if score > 60}
+# students_scores = {student:random.randint(1,100) for student in names}
+# passed_students = {student:score for (student, score) in students_scores.items() if score > 60}
 
-print(students_scores)
-print(passed_students)
+# print(students_scores)
+# print(passed_students)
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+# for (key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+# print(student_data_frame)
+
+# for (key, value) in student_data_frame.items():
+#     print(value)
+
+for (index, row) in student_data_frame.iterrows():
+    print(row.student)
