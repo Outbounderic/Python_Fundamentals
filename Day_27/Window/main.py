6411839f9fd97ab4ca617b0a402eb001cbf5fa95
Eric <ericvanmeter57@gmail.com>
2022-01-24 19:38:05 -0600

    Working with grid.

--- Day_27/Window/main.py
+++ Day_27/Window/main.py
@@ -5,30 +5,28 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 
+def button_clicked():
+    string_entry = input.get()
+    my_label.config(text=string_entry)
+
+
 # Label
 my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
+my_label.grid(column=0, row=0)
 
 my_label["text"] = "New text"
 my_label.config(text="New Text")
 
+
 # Button
-
-def button_clicked():
-    print("I got clicked!")
-    my_label.config(text="I got clicked!")
-
 button = Button(text="Click Me!", command=button_clicked)
-button.pack()
+button.grid(column=0, row=1)
 
+# Entry
 
-
-
-
-
-
-
-
-
+input = Entry(width=20)
+input.get()
+input.grid(column=0, row=2)
 
 window.mainloop()
