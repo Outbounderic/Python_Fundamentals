616aa40334eb6e77e3e62c9b67e0225e66ab7406
Eric <ericvanmeter57@gmail.com>
2022-01-24 20:28:58 -0600

    Created Mile to KM converter.

--- Day_27/Window/main.py
+++ Day_27/Window/main.py
@@ -1,32 +1,36 @@
 from tkinter import *
 
-window = Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+FONT_TYPE = ("Arial", 18, "bold")
 
 
 def button_clicked():
-    string_entry = input.get()
-    my_label.config(text=string_entry)
+    mile_value = mile_input.get()
+    mile_value *= 1.60934
+    km_output.config(text=round(mile_value, 1))
 
 
-# Label
-my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
-my_label.grid(column=0, row=0)
+window = Tk()
+window.title("Mile to KM")
+window.minsize(width=100, height=50)
+window.config(padx=10, pady=20)
 
-my_label["text"] = "New text"
-my_label.config(text="New Text")
+# create input for num and miles label for it
+mile_input = Entry(width=20)
+mile_input.grid(column=2, row=1)
 
+mile_label = Label(text="Miles", font=FONT_TYPE)
+mile_label.grid(column=3, row=1)
+# create box comparison string
+equal_label = Label(text="is equal to", font=FONT_TYPE)
+equal_label.grid(column=1, row=2)
+# create output number and km label
+km_output = Label(text=0, font=FONT_TYPE)
+km_output.grid(column=2, row=2)
 
-# Button
-button = Button(text="Click Me!", command=button_clicked)
-button.grid(column=0, row=1)
-
-# Entry
-
-input = Entry(width=20)
-input.get()
-input.grid(column=0, row=2)
+km_label = Label(text="KM", font=FONT_TYPE)
+km_label.grid(column=3, row=2)
+# create calculate button
+calculate_button = Button(text="Calculate", font=FONT_TYPE, command=button_clicked)
+calculate_button.grid(column=2, row=3)
 
 window.mainloop()
