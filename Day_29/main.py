774f9e14c12eabf17e0f7ba1a3d7bde1d54f40d4
Eric <ericvanmeter57@gmail.com>
2022-01-28 17:51:56 -0600

    Added canvas with logo.

--- Day_29/main.py
+++ Day_29/main.py
@@ -1,5 +1,24 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg="white")
+
+canvas = Canvas(width=240, height=240, bg="white", highlightthickness=0)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(120, 120, image=lock_img)
+canvas.pack()
+
+
+
+
+
+
+
+window.mainloop()
