e780532273d3d51ff28050ef4c6830592fb68520
Eric <ericvanmeter57@gmail.com>
2022-01-28 19:21:04 -0600

    Adjusted margins.

--- Day_29/main.py
+++ Day_29/main.py
@@ -8,32 +8,32 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20, bg="white")
+window.config(padx=50, pady=20, bg="white")
 
-canvas = Canvas(width=240, height=240, bg="white", highlightthickness=0)
+canvas = Canvas(width=200, height=200, bg="white", highlightthickness=0)
 lock_img = PhotoImage(file="logo.png")
-canvas.create_image(72, 120, image=lock_img)
+canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:", bg="white")
-website_label.grid(column=0, row=1, padx=2, pady=2)
-website_entry = Entry(width=43)
-website_entry.grid(column=1, row=1, padx=2, pady=2)
+website_label.grid(column=0, row=1)
+website_entry = Entry(width=50)
+website_entry.grid(column=1, row=1, columnspan=2)
 
 email_label = Label(text="Email/Username:", bg="white")
-email_label.grid(column=0, row=2, padx=2, pady=2)
-email_entry = Entry(width=43)
-email_entry.grid(column=1, row=2, padx=2, pady=2)
+email_label.grid(column=0, row=2)
+email_entry = Entry(width=50)
+email_entry.grid(column=1, row=2, columnspan=2)
 
 pass_label = Label(text="Password:", bg="white")
-pass_label.grid(column=0, row=3, padx=2, pady=2)
-pass_entry = Entry(width=24)
-pass_entry.grid(column=1, row=3, padx=2, pady=2, sticky="w")
-pass_button = Button(text="Generate Password", highlightthickness=0)
-pass_button.grid(column=1, row=3, sticky="e")
+pass_label.grid(column=0, row=3)
+pass_entry = Entry(width=32)
+pass_entry.grid(column=1, row=3)
+pass_button = Button(text="Generate Password")
+pass_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", highlightthickness=0, width=36)
-add_button.grid(column=1, row=4)
+add_button = Button(text="Add", width=36)
+add_button.grid(column=1, row=4, columnspan=2)
 
 
 window.mainloop()
