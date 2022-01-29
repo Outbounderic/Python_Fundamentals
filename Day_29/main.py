f12b41481658482889647363a686a63b0fac6a85
Eric <ericvanmeter57@gmail.com>
2022-01-28 19:35:15 -0600

    Created data file for pass.

--- Day_29/main.py
+++ Day_29/main.py
@@ -4,6 +4,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = pass_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        pass_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -19,11 +30,13 @@ website_label = Label(text="Website:", bg="white")
 website_label.grid(column=0, row=1)
 website_entry = Entry(width=50)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_label = Label(text="Email/Username:", bg="white")
 email_label.grid(column=0, row=2)
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "eric@notrealgmail.com")
 
 pass_label = Label(text="Password:", bg="white")
 pass_label.grid(column=0, row=3)
@@ -32,7 +45,7 @@ pass_entry.grid(column=1, row=3)
 pass_button = Button(text="Generate Password")
 pass_button.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
 
 
