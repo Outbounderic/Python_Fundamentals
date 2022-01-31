42025f8d18d0e9a9d7da4a6469bbf0e6ab05fbc2
Eric <ericvanmeter57@gmail.com>
2022-01-30 21:34:30 -0600

    Working with error handling.

---
+++ Day_30/main.py
@@ -0,0 +1,16 @@
+#
+#
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"That key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is an error that I made up.")
+
