5bba262509e3f1621f9416cd3691ab5d9b0aac68
Eric <ericvanmeter57@gmail.com>
2022-02-03 21:20:41 -0600

    Created basic layout for flashcards.

--- Day_31/main.py
+++ Day_31/main.py
@@ -3,5 +3,37 @@ from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
+window.title("Flash Card")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(width=800, height=526, highlightthickness=0)
+flash_card_front = PhotoImage(file="./images/card_front.png")
+canvas.create_image(400, 263, image=flash_card_front)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_button_image = PhotoImage(file="./images/wrong.png")
+wrong_button = Button(image=wrong_button_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+right_button_image = PhotoImage(file="./images/right.png")
+right_button = Button(image=right_button_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
+
+flash_card_back = PhotoImage(file="./images/card_back.png")
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.mainloop()
