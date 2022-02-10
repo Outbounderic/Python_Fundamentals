d877242719c347d27cb8da10f4eacdef852192b6
Eric <ericvanmeter57@gmail.com>
2022-02-10 14:55:38 -0600

    Formatted connection.

--- Day_32/main.py
+++ Day_32/main.py
@@ -3,8 +3,12 @@ from password import my_password
 
 my_email = "tomfizzlery@gmail.com"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs="tiddlefizzlery@yahoo.com", msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="tiddlefizzlery@yahoo.com",
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
+
