5ba1cb1d0d83486bb1d789b59ce215307aa01d71
Eric <ericvanmeter57@gmail.com>
2022-02-10 15:05:49 -0600

    Added several tokens for testing.

--- Day_32/main.py
+++ Day_32/main.py
@@ -1,14 +1,18 @@
 import smtplib
 from password import my_password
 
-my_email = "tomfizzlery@gmail.com"
+# my_email = "tomfizzlery@gmail.com"
+my_email = "tiddlefizzlery@yahoo.com"
+password = "ijsshkqznzlhtuzz"
+gmail = "smtp.gmail.com"
+yahoo = "smtp.mail.yahoo.com"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
+with smtplib.SMTP(yahoo) as connection:
     connection.starttls()
-    connection.login(user=my_email, password=my_password)
+    connection.login(user=my_email, password=password)
     connection.sendmail(
         from_addr=my_email,
-        to_addrs="tiddlefizzlery@yahoo.com",
+        to_addrs="tomfizzlery@gmail.com",
         msg="Subject:Hello\n\nThis is the body of my email."
     )
 
